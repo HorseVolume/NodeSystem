d093201f52194828b033242a81e63d3c1e8e0334
Ty Underwood <tyisreally@gmail.com>
2015-04-15 20:30:24 -0400

    added mouselook

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class Controller : MonoBehaviour {
 
+	//peek circle
+	public GameObject peekCirlce;
+
+	//mouselook
+	public GameObject mainCamera;
+	public GameObject user;
+
+	private Vector3 mousePosition;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +19,11 @@ public class Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		mousePosition = mainCamera.GetComponent<Camera> ().ScreenToWorldPoint (Input.mousePosition);
+
+		mainCamera.transform.position = new Vector3 ((user.transform.position.x + mousePosition.x) / 2, (user.transform.position.y + mousePosition.y) / 2, mainCamera.transform.position.z);
+
+
 	}
 }
